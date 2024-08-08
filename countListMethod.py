685f6080229125664fca6f63b9eee3bdaed03e8f
Milton Navarro <milton.j.navarro.p@gmail.com>
2024-08-08 12:03:54 -0600

    count list method

---
+++ countListMethod.py
@@ -0,0 +1,5 @@
+names = ['John', 'Paul', 'George', 'Ringo', 'John', 'Paul', 'George', 'Ringo', 'John', 'Paul', 'George', 'Ringo', 'John', 'Paul', 'George', 'Ringo', 'John', 'Paul', 'George', 'Ringo']
+
+nameToBeFound = 'John'
+
+print("The name " + nameToBeFound + " was found " + str(names.count('John')) + " times in the list")
