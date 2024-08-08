b33c740c9102a07410c07d5253ee60ef5a9fa08c
Milton Navarro <milton.j.navarro.p@gmail.com>
2024-08-08 12:09:15 -0600

    extend list method

---
+++ extendListMethod.py
@@ -0,0 +1,11 @@
+dragonBallZCharacters = ['Goku','Vegeta']
+
+earthlings = ['Krillin','Yamcha']
+
+print("Dragon Ball Z Characters", dragonBallZCharacters)
+
+dragonBallZCharacters.extend(earthlings)
+
+print("Dragon Ball Z Characters extended with Earthlings", dragonBallZCharacters)
+
+
