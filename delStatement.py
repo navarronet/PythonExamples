a5009702ffa17450d3917b33b8db301bed2549bb
Milton Navarro <milton.j.navarro.p@gmail.com>
2024-08-08 10:50:54 -0600

    first commit

---
+++ delStatement.py
@@ -0,0 +1,7 @@
+names = ['John', 'Paul', 'George', 'Ringo', 'John', 'Paul', 'George', 'Ringo', 'John', 'Paul', 'George', 'Ringo', 'John', 'Paul', 'George', 'Ringo', 'John', 'Paul', 'George', 'Ringo']
+
+print(names)
+
+del names[1]
+
+print("list without the second item", names)
