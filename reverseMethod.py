c9ec6075b351cb95109bde1853dd024fc2061c87
Milton Navarro <milton.j.navarro.p@gmail.com>
2024-08-18 09:10:52 -0600

    reverse method

---
+++ reverseMethod.py
@@ -0,0 +1,9 @@
+IDEs = ['vscode', 'pycharm', 'sublime', 'atom', 'pycharm', 'notepad++', 'visual studio','android studio','xcode']
+
+# reverse the list
+
+print(IDEs) # ['vscode', 'pycharm', 'sublime', 'atom', 'pycharm', 'notepad++', 'visual studio', 'android studio', 'xcode']
+
+IDEs.reverse()
+
+print(IDEs) # ['xcode', 'android studio', 'visual studio', 'notepad++', 'pycharm', 'atom', 'sublime', 'pycharm', 'vscode']
