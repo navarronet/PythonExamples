72866936441c91b03fde4f373b81512ebe1e4020
Milton Navarro <milton.j.navarro.p@gmail.com>
2024-08-18 09:09:35 -0600

    remove method

---
+++ removeMethod.py
@@ -0,0 +1,7 @@
+IDEs = ['vscode', 'pycharm', 'sublime', 'atom', 'pycharm', 'notepad++', 'visual studio','android studio','xcode']
+
+# remove the element
+
+IDEs.remove('pycharm')
+
+print(IDEs) # ['vscode', 'sublime', 'atom', 'pycharm', 'notepad++', 'visual studio', 'android studio', 'xcode']
