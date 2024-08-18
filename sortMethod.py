78f9bad5050c9a4a2e4de278998d95e6df339a12
Milton Navarro <milton.j.navarro.p@gmail.com>
2024-08-18 09:12:53 -0600

    sort method

---
+++ sortMethod.py
@@ -0,0 +1,9 @@
+IDEs = ['vscode', 'pycharm', 'sublime', 'atom', 'pycharm', 'notepad++', 'visual studio','android studio','xcode']
+
+print(IDEs) # ['vscode', 'pycharm', 'sublime', 'atom', 'pycharm', 'notepad++', 'visual studio', 'android studio', 'xcode']
+
+# sort the list
+
+IDEs.sort()
+
+print(IDEs) # ['android studio', 'atom', 'notepad++', 'pycharm', 'pycharm', 'sublime', 'visual studio', 'vscode', 'xcode']
