603a03a77ecaceffde7cb55838f436000c9f8ac9
Milton Navarro <milton.j.navarro.p@gmail.com>
2024-08-18 09:07:40 -0600

    insert method

---
+++ insertMethod.py
@@ -0,0 +1,7 @@
+IDEs = ['vscode', 'pycharm', 'sublime', 'atom', 'pycharm', 'notepad++', 'visual studio','android studio','xcode']
+
+# insert the element at the specified index
+
+IDEs.insert(1, 'eclipse')
+
+print(IDEs) # ['vscode', 'eclipse', 'pycharm', 'sublime', 'atom', 'pycharm', 'notepad++', 'visual studio', 'android studio', 'xcode']
